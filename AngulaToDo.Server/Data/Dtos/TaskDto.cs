2e6e3987a3b5ec90b1cec4b7cf78a72987f95e85
семенец Иван <ivan.g.sem@gmail.com>
2024-10-23 03:32:05 +0300

    small changes

--- AngulaToDo.Server/Data/Dtos/TaskDto.cs
+++ AngulaToDo.Server/Data/Dtos/TaskDto.cs
@@ -5,7 +5,7 @@
         public string? Title { get; set; }
         public string? Description { get; set; }
         public bool? IsImportant { get; set; }
-        public bool? Coompleted { get; set; }
+        public bool? Completed { get; set; }
         public DateTime Created { get; set; }
         public DateTime DueDate { get; set; }
     }
--- angulatodo.client/src/app/services/task.service.ts
+++ angulatodo.client/src/app/services/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,19 @@ export class TaskService {
 
   constructor(private http: HttpClient) { }
 
-  getAllTasks(userId: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.tasksBaseUrl}/${userId}`);
+  getAllTasks(userId: string): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.tasksBaseUrl}/${userId}`);
+  }
+  getTaskById(userId: string, taskId: number): Observable<Task> {
+    return this.http.get<Task>(`${this.tasksBaseUrl}/${userId}/${taskId}`);
+  }
+  createTask(userId: string, task: Task): Observable<Task> {
+    return this.http.post<Task>(`${this.tasksBaseUrl}/${userId}`, task);
+  }
+  updateTask(userId: string, taskId: number, task: Task): Observable<void> {
+    return this.http.put<void>(`${this.tasksBaseUrl}/${userId}/${taskId}`, task);
+  }
+  deleteTask(userId: string, taskId: number): Observable<void> {
+    return this.http.get<void>(`${this.tasksBaseUrl}/${userId}/${taskId}`);
   }
 }
